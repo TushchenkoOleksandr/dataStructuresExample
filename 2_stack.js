ca8f518bcd591a312eb994d0d7f69b9860fdeaaf
Oleksandr Tyshchenko <o.tyshchenko@spd-ukraine.com>
2021-09-23 17:52:50 +0300

    add stack

---
+++ 2_stack.js
@@ -0,0 +1,17 @@
+console.log('stack');
+const stack = [];
+
+stack.push(1);
+console.log(stack);
+stack.push(2);
+console.log(stack);
+stack.pop();
+console.log(stack);
+stack.push(3);
+console.log(stack);
+stack.push(4);
+console.log(stack);
+stack.pop();
+console.log(stack);
+stack.pop();
+console.log(stack);
