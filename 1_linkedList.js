086f2333ae208a67a696c9d39622d2fe2adf9fc5
Oleksandr Tyshchenko <o.tyshchenko@spd-ukraine.com>
2021-09-23 17:59:51 +0300

    add queue

--- 1_linkedList.js
+++ 1_linkedList.js
@@ -1,3 +1,4 @@
+console.log('linked list')
 class LinkedList {
   constructor() {
     this.root = null;
---
+++ 3_ queue.js
@@ -0,0 +1,20 @@
+console.log('queue');
+
+const queue = [];
+
+queue.push(1)
+console.log(queue);
+queue.push(2)
+console.log(queue);
+queue.push(3)
+console.log(queue);
+queue.push(4)
+console.log(queue);
+queue.shift()
+console.log(queue);
+queue.push(5)
+console.log(queue);
+queue.shift()
+console.log(queue);
+queue.shift()
+console.log(queue);
