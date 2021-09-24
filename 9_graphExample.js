f4725ee596f0c2310e95d5881f280a73b2a0e00f
Oleksandr Tyshchenko <o.tyshchenko@spd-ukraine.com>
2021-09-24 12:53:23 +0300

    add graph example

---
+++ 9_graphExample.js
@@ -0,0 +1,8 @@
+console.log("graph");
+
+const graphExample = {
+  a: { id: 1, name: "a", friends: [this.b, this.c] },
+  b: { id: 2, name: "b", friends: [this.d] },
+  c: { id: 3, name: "c", friends: [this.a] },
+  d: { id: 4, name: "d", friends: [this.a, this.b, this.c] },
+};
